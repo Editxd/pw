083f5a1d74ff7a5093d3149494c44b0d322b8d7a
Editxd <152770568+Editxd@users.noreply.github.com>
2023-12-12 15:56:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def account_login(bot: Client, m: Message):
         "Send **Number or email** in this manner otherwise bot will not respond.\n\nSend like this:-  **mobile no.**"
     )  
     input1: Message = await bot.listen(editable.chat.id)
-    raw_text1=str(input1.text)
+    raw_text1=input1
     
 headers={
 "Host": "api.penpencil.xyz",
